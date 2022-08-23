3880eca179d063ca13950b4d50f5f564de1da2bc
stefkoxd <mitrevskistefan69@hotmail.com>
2022-08-23 14:16:26 +0200

    wip: rtc with client and server initial impl

--- public/script.js
+++ public/script.js
@@ -1,5 +1,49 @@
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-unused-vars
 let myVideoStream
+
+const socket = io('/')
+
+const peer = new Peer(undefined, {
+  path: '/peerjs',
+  host: 'localhost',
+  port: '9000',
+})
+
+const videoGrid = document.getElementById('video-grid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices
+  .getUserMedia({
+    audio: true,
+    video: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream
+    addVideoStream(myVideo, stream)
+
+    peer.on('call', (call) => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+
+    socket.on('user-connected', (userId) => {
+      console.log('connected')
+      connectToNewUser(userId, stream)
+    })
+  })
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', (userVideoStream) => {
+    addVideoStream(video, userVideoStream)
+  })
+}
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream
@@ -9,15 +53,10 @@ const addVideoStream = (video, stream) => {
   })
 }
 
-const videoGrid = document.getElementById('video-grid')
-const myVideo = document.createElement('video')
-myVideo.muted = true
-navigator.mediaDevices
-  .getUserMedia({
-    audio: true,
-    video: true,
-  })
-  .then((stream) => {
-    myVideoStream = stream
-    addVideoStream(myVideo, stream)
-  })
+// TODO: fix random user
+const user = `user-${Math.floor(Math.random() * 100)}`
+
+peer.on('open', (id) => {
+  console.log('peer open')
+  socket.emit('join-room', ROOM_ID, id, user)
+})
--- server.js
+++ server.js
@@ -8,6 +8,31 @@ nunjucks.configure('views', {
   express: app,
 })
 app.set('view engine', 'njk')
+
+// Socket.io allows us to do real-time communication
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+})
+
+// PeerJS allows us to implement WebRTC
+const { PeerServer } = require('peer')
+
+// eslint-disable-next-line no-unused-vars
+const peerServer = PeerServer({
+  debug: true,
+  port: 9000,
+  path: '/peerjs',
+})
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
+  })
+})
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
