cb22a1111bbd69c9b1e11ce8c1acce96067ff9c4
stefkoxd <mitrevskistefan69@hotmail.com>
2022-08-25 11:27:48 +0200

    feat: introduced webpack

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const isDev = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  context: __dirname,
+  devtool: isDev ? 'source-map' : 'nosources-source-map',
+  entry: {
+    scripts: './client-scripts/index.js',
+    styles: './stylesheets/index.scss',
+  },
+  output: {
+    path: path.resolve('./public'),
+    filename: '[name].js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  require.resolve('@babel/preset-env'),
+                  {
+                    targets: 'ie >= 11',
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [autoprefixer()],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: { implementation: require('sass') },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.scss', '.css', '.js', '.njk'],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [path.resolve('./public')],
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+}
